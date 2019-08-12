14c7c10949c0cbf9263f820d278b5fbe33074c65
王洋洋SY800160 <wangyangyang@odc.cmbchina.cn>
2019-08-12 19:19:27 +0800

    初始化工程

---
+++ WYZhiHuNews/WYZhiHuNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WYZhiHuNews
+//
+//  Created by wangyangoc on 2019/8/12.
+//  Copyright © 2019 wangyangoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
